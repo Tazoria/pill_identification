bafd60e2dccbad47326efd47f3c5761f5de73466
Tazoria <gayeonlee93@gmail.com>
2023-10-11 21:21:41 +0900

    추론 구현

--- web/models/inference.py
+++ web/models/inference.py
@@ -1,41 +1,56 @@
+from tqdm import tqdm
 import torch
-import torchvision
-from web.models.custom_models import CustomMobileNetV3Large
+from custom_models import CustomMobileNetV3Large
+from get_dataloader import get_test_loader
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-def infer(image_tensor):
-    # 모델 불러와 적용하기
-    model = CustomMobileNetV3Large(num_classes=500)
-    optimizer = torch.optim.NAdam(model.parameters(), lr=0.001)
+def inference(model=CustomMobileNetV3Large(num_classes=500), is_grayscale=False):
+  # 데이터 불러오기
+  test_dir = r'D:/data/training/sources/test'
 
-    checkpoint = torch.load('models/save/mobilenet_sample.pth')
-    model.load_state_dict(checkpoint['model_state_dict'])  # 모델 가중치 불러오기
+  # 모델 로드
+  if is_grayscale:
+    checkpoint = torch.load('save/mobilenet_epoch10_batch128_pretrained_Augmentated_Grayscaled.pth')
+    test_loader = get_test_loader(test_dir, batch_size=126, is_train=False, is_grayscale=is_grayscale)
+  else:
+    test_loader = get_test_loader(test_dir, batch_size=126, is_train=False, is_grayscale=is_grayscale)
+    checkpoint = torch.load('save/mobilenet_epoch10_batch128_pretrained_Augmentated.pth')
+  model.load_state_dict(checkpoint['model_state_dict'])  # 모델 가중치 불러오기
+  model.to(device)
 
-    # 모델을 평가 모드로 설정
-    model.eval()
-    outputs = model(image_tensor)
-    values, indices = torch.topk(outputs, k=5)
-    predicted_label = values.item()
-
-    # 원래 클래스 이름 아는방법 찾기
-    # ImageFolder의 클래스 레이블과 클래스 이름 간의 매핑 정보 확인
-    class_to_idx = dataset.class_to_idx
-
-    # 클래스 레이블을 클래스 이름으로 변환
-    predicted_classname = list(class_to_idx.keys())[list(class_to_idx.values()).index(predicted_label)]
-
-
-
-
+  with torch.no_grad():
     correct = 0
     total = 0
-    with torch.no_grad():
-      for inputs, labels in tqdm_notebook(test_dataloader):
-        inputs, labels = inputs.to(device), labels.to(device)
-        outputs = model(inputs)
-        _, predicted = torch.max(outputs, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    top5_correct = 0
 
-    accuracy = 100 * correct / total
-    print(f'Test Accuracy: {accuracy}')
+    for batch_idx, (inputs, labels) in enumerate(test_loader):
+      inputs, labels = inputs.to(device), labels.to(device)
+      outputs = model(inputs)
+      # top-1 정확도
+      _, predicted = torch.max(outputs, 1)
+      total += labels.size(0)
+      correct += (predicted == labels).sum().item()
+
+      # top-5 정확도 계산
+      _, top5_preds = torch.topk(outputs, 5)
+      top5_correct += sum(labels.view(-1, 1) == predicted).sum().item()
+
+  top1_accuracy = round(100 * correct / total, 2)
+  top5_accuracy = round(100 * top5_correct / total, 2)
+
+  accuracy_dict = {
+    'top1': top1_accuracy,
+    'top5': top5_accuracy}
+
+  print(f'Test Top-1 Accuracy: {accuracy_dict["top1"]}')
+  print(f'Test Top-5 Accuracy: {accuracy_dict["top5"]}')
+
+  return accuracy_dict
+
+
+if __name__ == '__main__':
+  accuracy_dict = inference(is_grayscale=False)
+  print('=' * 20)
+  accuracy_dict_grayscale = inference(is_grayscale=True)
