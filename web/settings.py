58febba4f59aec961b8434a65a193c5d26031696
Tazoria <gayeonlee93@gmail.com>
2023-10-16 15:14:06 +0900

    template 경로 에러 수정

--- web/settings.py
+++ web/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'web'/ 'templates'],
+        'DIRS': [BASE_DIR / 'web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
